fc0650eb84674801734c55611cbe3de4a30a6199
Megan Oliver <rms.moliver@gmail.com>
2017-05-12 07:52:31 -0700

    Initial live search upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+/* To do:
+  1. Read what's typed into the searchbox (lowercase).
+  2. Compare that to the "data-title" attributes.
+  3. If they match, show the img, else take picture away.
+  4. If no match, alert that there are no matches.
+*/
 
+var search
+var captions = []
+
+
+//Take input & put into var (lowercase)
+$('#search-box').on('keyup', function() {
+  var search = $(this).val();
+  search = search.toLowerCase();
+  console.log(search); //Testing that it works
+});
+
+//Put all img & captions in an array
+
+var captions = $("img").toArray();
+console.log(captions); //Testing that it works
+
+//Loop through the array
+
+for (var i = 0; i < captions.length; i += 1) {
+  if (search === captions) {
+    $("captions").show()
+  }
+  else {
+    $("captions").hide()
+  }
+}
